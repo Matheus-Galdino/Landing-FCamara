5ee0b572a349e5fd326d017ce436b93dd757ea07
Matheus Carvalho <matheus10277@gmail.com>
2021-10-06 09:55:55 -0300

    Implementando burger menu para mobile

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelectorAll(".nav-link");
+const navLinksContainer = document.querySelector(".nav-links");
 const accordions = document.querySelectorAll(".accordion");
 
 for (let i = 0; i < accordions.length; i++) {
@@ -7,3 +10,18 @@ for (let i = 0; i < accordions.length; i++) {
     e.target.classList.toggle("active");
   });
 }
+
+for (let i = 0; i < navLinks.length; i++) {
+  const element = navLinks[i];
+  
+  element.addEventListener("click", () => {
+    navLinksContainer.classList.remove("open");
+    burger.classList.remove("open");
+  })
+}
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("open");
+
+  navLinksContainer.classList.toggle("open");
+})
